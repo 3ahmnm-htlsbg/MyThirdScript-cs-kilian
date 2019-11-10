9a198f311630353d5cd9d0666475aee7152dc99f
Kilian Reisinger <kilian.reisinger02@htl-salzburg.ac.at>
2019-11-10 12:19:03 +0100

    add int declarations and Debug Logs with calculations

--- Assets/MyGame/Scripts/AddNumbers.cs
+++ Assets/MyGame/Scripts/AddNumbers.cs
@@ -7,6 +7,14 @@ public class AddNumbers : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int a = 3;
+        int b = 4;
+
+        Debug.Log("a = " + a);
+        Debug.Log("b = " + b);
+        Debug.Log("a + b = " + (a + b));
+
+        Debug.Log("add numbers a = " + a + ", b = " + b + "; a + b = " + (a + b));
         
     }
 
@@ -16,3 +24,4 @@ public class AddNumbers : MonoBehaviour
         
     }
 }
+ 
